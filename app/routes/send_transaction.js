f2fb53792e9ee0fa4c38d0946d66ba02ff5480f5
Evgeniy.Taburyanskiy <Evgeniy.Taburyanskiy@gmail.com>
2019-03-23 02:04:57 +0300

    send_transaction endpoint mock

--- app/routes/send_transaction.js
+++ app/routes/send_transaction.js
@@ -3,25 +3,40 @@ const router = new Router();
 const logger = require('../winston')(module);
 
 const oTxResult = {
-  "jsonrpc": "2.0",
-  "id": "",
-  "result": {
-    "code": 0,
-    "data": "",
-    "log": "",
-    "hash": "A535D002C4E3E1DE2133F8F778364F668AA9119B9C52F16F2685D21BDC1D240E"
+  'OK' : {
+    'jsonrpc': '2.0',
+    'id'     : '',
+    'result' : {
+      'code': 0,
+      'data': '',
+      'log' : '',
+      'hash': 'A535D002C4E3E1DE2133F8F778364F668AA9119B9C52F16F2685D21BDC1D240E'
+    }
+  },
+  'ERR': {
+    'jsonrpc': '2.0',
+    'id'     : '',
+    'error'  : {
+      'code'     : 412,
+      'message'  : 'Check tx error',
+      'tx_result': {
+        'code': 114,
+        'log' : 'Gas price of tx is too low to be included in mempool. Expected 10'
+      }
+    }
   }
+
 };
 
 router.get('/:tx', (req, res, next) => {
   // do something with req.body
-  res.send(oTxResult);
-/*
-  res.setTimeout(1000*5, function() {
-    res.send({params: req.params});
-    logger.debug(JSON.stringify({params: req.params}));
-  });
-*/
+  res.send(oTxResult.OK);
+  /*
+    res.setTimeout(1000*5, function() {
+      res.send({params: req.params});
+      logger.debug(JSON.stringify({params: req.params}));
+    });
+  */
   //logger.debug(JSON.stringify({params: req.params}));
 
   return next();
@@ -29,13 +44,13 @@ router.get('/:tx', (req, res, next) => {
 
 router.get('', (req, res, next) => {
   //res.send({query: req.query});
-  res.send(oTxResult);
-/*
-  res.setTimeout(10000*5, function() {
-    res.send({query: req.query});
-    logger.debug(JSON.stringify({query: req.query}));
-  });
-*/
+  res.send(oTxResult.OK);
+  /*
+    res.setTimeout(10000*5, function() {
+      res.send({query: req.query});
+      logger.debug(JSON.stringify({query: req.query}));
+    });
+  */
   next();
 });
 
